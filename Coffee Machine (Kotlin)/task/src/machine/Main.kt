515be4f36c0b98e8d6c1ef648f6ba676f229dfbd
Iakovenko Vladimir <yakovenko.vladimir95@yandex.ru>
2025-06-14 16:46:58 +0300

    Second Task

--- Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -1,5 +1,18 @@
-package machine
+import java.util.Scanner
+
+const val WATER = 200
+const val MILK = 50
+const val COFFEE_BEANS = 15
 
 fun main() {
-    println("Hello, world!")
+    val scanner = Scanner(System.`in`)
+
+    println("Write how many cups of coffee you will need:")
+    val countCups = scanner.nextInt()
+
+    println("For $countCups of coffee you will need:")
+    println("${countCups * WATER} ml of water")
+    println("${countCups * MILK} ml of milk")
+    println("${countCups * COFFEE_BEANS} g of coffee beans")
+
 }
