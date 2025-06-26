63a6158c11b86045208a178c33110a5e55aebd62
Iakovenko Vladimir <yakovenko.vladimir95@yandex.ru>
2025-06-26 23:45:23 +0300

    Four Task

--- Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -1,36 +1,76 @@
-import java.util.Scanner
-
-const val WATER = 200
-const val MILK = 50
-const val COFFEE_BEANS = 15
-
 fun main() {
-    val scanner = Scanner(System.`in`)
+    val machine = CoffeeMachine()
+    machine.printState()
 
-    println("Write how many ml of water the coffee machine has:")
-    val water = scanner.nextInt()
-    println("Write how many ml of milk the coffee machine has:")
-    val milk = scanner.nextInt()
-    println("Write how many grams of coffee beans the coffee machine has:")
-    val coffeeBeans = scanner.nextInt()
-    println("Write how many cups of coffee you will need:")
-    val countCups = scanner.nextInt()
+    println("Write action (buy, fill, take):")
+    val action = readln()
 
-    val countCupsCanWeMake: (Int, Int, Int) -> Int = { waterValue, milkValue, coffeeBeansValue ->
-        val waterCondition = waterValue / WATER
-        val milkCondition = milkValue / MILK
-        val coffeeBeansCondition = coffeeBeansValue / COFFEE_BEANS
-        var res = waterCondition
-        res = if (milkCondition < res) milkCondition else res
-        res = if (coffeeBeansCondition < res) coffeeBeansCondition else res
-        res
+    when (action) {
+        "buy" -> buy(machine)
+        "fill" -> fill(machine)
+        "take" -> take(machine)
     }
 
-    val count = countCupsCanWeMake(water, milk, coffeeBeans)
+    machine.printState()
+}
 
-    when (countCups.compareTo(count)) {
-        -1 -> println("Yes, I can make that amount of coffee (and even ${count - countCups} more than that)")
-        1 -> println("No, I can make only $count cups of coffee")
-        else -> println("Yes, I can make that amount of coffee")
+class CoffeeMachine {
+    var water = 400
+    var milk = 540
+    var coffeeBeans = 120
+    var disposableCups = 9
+    var money = 550
+
+    fun printState() {
+        println("The coffee machine has:")
+        println("$water ml of water")
+        println("$milk ml of milk")
+        println("$coffeeBeans g of coffee beans")
+        println("$disposableCups disposable cups")
+        println("$$money of money")
     }
+
+    fun sellCoffee(coffee: Coffee) {
+        water -= coffee.water
+        milk -= coffee.milk
+        coffeeBeans -= coffee.coffeeBeans
+        money += coffee.price
+        disposableCups--
+    }
+}
+
+fun take(machine: CoffeeMachine) {
+    println("I gave you ${machine.money}")
+    machine.money = 0
+}
+
+fun buy(machine: CoffeeMachine) {
+    println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+    val option = readln().toInt()
+    machine.sellCoffee(
+        when (option) {
+            1 -> Coffee.ESPRESSO
+            2 -> Coffee.LATTE
+            3 -> Coffee.CAPPUCCINO
+            else -> throw IllegalArgumentException("Unexpected option")
+        }
+    )
+}
+
+fun fill(machine: CoffeeMachine) {
+    println("Write how many ml of water you want to add:")
+    machine.water += readln().toInt()
+    println("Write how many ml of milk you want to add:")
+    machine.milk += readln().toInt()
+    println("Write how many grams of coffee beans you want to add:")
+    machine.coffeeBeans += readln().toInt()
+    println("Write how many disposable cups you want to add:")
+    machine.disposableCups += readln().toInt()
+}
+
+
+enum class Coffee(val water: Int, val milk: Int, val coffeeBeans: Int, val price: Int) {
+    ESPRESSO(250, 0, 16, 4),
+    LATTE(350, 75, 20, 7),
+    CAPPUCCINO(200, 100, 12, 6),
 }
